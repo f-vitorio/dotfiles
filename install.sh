41b2a9b3fcae74a0eaacd38b99553e61ca5e3e35
fvitorio <fabio.vitorios@gmail.com>
2021-06-19 17:56:10 -0300

    ebook reader

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
-#update
-sudo apt-get update
+#!/bin/bash
 
+
+#-------------------------------------------------------
+# script para instalação dos meus softwares linux
+
+#-------------------------------------------------------
+
+
+
+
+#update
+sudo apt-get -f install
+sudo apt-get update
+sudo apt-get autoremove
+sudo apt-get dist-upgrade
 #git
 sudo apt-get install git -y
 
@@ -14,4 +27,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #instal skype
 sudo snap install skype --classic
 
+#Discord
+sudo snap install discord
 
+#install ebook reader
+sudo apt install calibre
